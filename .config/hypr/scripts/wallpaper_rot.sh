eb2467d4c6fdf618796af0a00f007b164e2e54e8
TheWired <darth62969@gmail.com>
2024-10-23 11:14:52 -0400

    updated waybar and wallpaper configs.

--- .config/hypr/scripts/wallpaper_rot.sh
+++ .config/hypr/scripts/wallpaper_rot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 while true; do
-    waypaper --random
-    sleep 190
+    waypaper --random --state-file ~/.config/hypr/scripts/dp1.ini
+    waypaper --random --state-file ~/.config/hypr/scripts/dp2.ini
+    waypaper --random --state-file ~/.config/hypr/scripts/hdmi1.ini
+    sleep 300
 done
